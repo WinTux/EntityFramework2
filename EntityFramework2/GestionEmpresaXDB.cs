d33d3b822e5abfdb4bdd028ac3559e70227f06fc
rusokverse <rusokverse@MACBOOKPRO>
2022-09-16 19:31:29 -0400

    Creando el DbContext

--- EntityFramework2/GestionEmpresaXDB.cs
+++ EntityFramework2/GestionEmpresaXDB.cs
@@ -1,7 +1,9 @@
 ﻿using EntityFramework2.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EntityFramework2
@@ -11,10 +13,21 @@ namespace EntityFramework2
         public DbSet<Estudiante> Estudiantes { get; set; }
         public DbSet<Telefono> Telefonos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder opBuilder) {
-            if (!opBuilder.IsConfigured) { 
-                //TODO llenar con configuraciones
+            if (!opBuilder.IsConfigured) {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appconfig.json");
+                var conf = builder.Build();
+                opBuilder.UseSqlServer(conf["ConnectionStrings:miConexion"]);
             }
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Telefono>(entidad => {
+                entidad.HasOne(t => t.estudiante).WithMany(est => est.telefonos)
+                .HasForeignKey(t => t.codigoEst)
+                .HasConstraintName("FK_Telefono_Estudiante");
+            });
+        }
     }
 }
