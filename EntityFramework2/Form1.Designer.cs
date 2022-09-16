dbbee77f570cd891322321d53c1771805edb3d27
rusokverse <rusokverse@MACBOOKPRO>
2022-09-16 19:55:33 -0400

    Cambio de texto

--- EntityFramework2/Form1.Designer.cs
+++ EntityFramework2/Form1.Designer.cs
@@ -36,20 +36,20 @@
             // 
             this.button1.Location = new System.Drawing.Point(22, 18);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(116, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Obtener telefonos";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 69);
+            this.label1.Location = new System.Drawing.Point(22, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(62, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Resultado:";
             // 
             // Form1
             // 
